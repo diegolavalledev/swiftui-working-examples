5932542ad4dd90e1e9650b2b7a4acdea6d326a84
diegolavalledev <58344228+diegolavalledev@users.noreply.github.com>
2019-09-14 12:00:11 +0000

    SwiftUI Scroll Magic 2

---
+++ Sources/WorkingExamples/no-uiimage/JumpingTitleBar.swift
@@ -0,0 +1,109 @@
+import SwiftUI
+
+public struct JumpingTitleBar: View {
+  
+  private struct OffsetKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+      value = nextValue()
+    }
+  }
+
+  @State var onTop = true
+
+  var scrollView: some View {
+    GeometryReader { g in
+      ScrollView(.vertical) {
+        Text("""
+        Scroll down to see the title bar jump from top to bottom.
+        
+        Scroll down to see the title bar jump from top to bottom.
+        
+        Scroll down to see the title bar jump from top to bottom.
+        
+        Scroll down to see the title bar jump from top to bottom.
+        
+        Scroll down to see the title bar jump from top to bottom.
+        
+        Scroll down to see the title bar jump from top to bottom.
+        
+        Scroll down to see the title bar jump from top to bottom.
+        
+        Scroll down to see the title bar jump from top to bottom.
+        
+        Scroll down to see the title bar jump from top to bottom.
+        
+        Scroll down to see the title bar jump from top to bottom.
+        
+        Scroll down to see the title bar jump from top to bottom.
+        
+        Scroll down to see the title bar jump from top to bottom.
+        
+        Scroll down to see the title bar jump from top to bottom.
+        
+        Scroll down to see the title bar jump from top to bottom.
+        
+        Scroll down to see the title bar jump from top to bottom.
+        """)
+        .padding(.top, 60)
+        .padding(.bottom, 50)
+        .anchorPreference(key: OffsetKey.self, value: .top) {
+          g[$0].y
+        }
+      }
+    }
+  }
+
+  var topBar: some View {
+    VStack {
+      Text("Title Bar")
+      .font(.largeTitle)
+      Text("On top ☝️")
+    }
+    .frame(maxWidth: .infinity)
+    .padding(.top, 35)
+    .background(Color.white)
+  }
+  
+  var bottomBar: some View {
+    (Text("Title Bar").bold() + Text(" at the bottom 👇"))
+    .frame(maxWidth: .infinity)
+    .padding()
+    .background(Color.white)
+    .opacity(onTop ? 0 : 0.85)
+  }
+  
+  public init() {}
+
+  public var body: some View {
+    ZStack {
+      scrollView
+      .onPreferenceChange(OffsetKey.self) {
+        if $0 < -10 {
+          withAnimation {
+            self.onTop = false
+          }
+        } else {
+          withAnimation {
+            self.onTop = true
+          }
+        }
+      }
+      VStack {
+        if onTop {
+          topBar
+          .transition(.move(edge: .top))
+        }
+        Spacer()
+        bottomBar
+      }
+      .edgesIgnoringSafeArea([.top, .bottom])
+    }
+  }
+}
+
+struct JumpingTitleBar_Previews: PreviewProvider {
+  static var previews: some View {
+    JumpingTitleBar()
+  }
+}
